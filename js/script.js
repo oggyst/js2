043b377f19c551c6e182f68b3104187277fc54fc
oggyst <51537934+oggyst@users.noreply.github.com>
2019-10-10 14:18:07 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+function run(max , min) 
+{   
+    const intNumberOfRows = document.getElementById('numberOfColumns').value;
+    const intNumberOfColumns = document.getElementById('numberOfRows').value;
+    if(intNumberOfRows == "" || intNumberOfColumns == "" )
+    {
+        alert('Please fill required fields.');
+    } else if (intNumberOfRows < 1 || intNumberOfColumns < 1)
+        alert('All numbers must be 1 or greater');
+    else
+        {
+        let arrRowNumbers = [];
+        let arrColumnNumbers = [];
+        let arrMatrix = [[]];
+        let intArrayAverage = 0;
+        document.write ('<style>td{border:1px solid black;}</style><table><tr><td></td>');
+        for (var i = 0; i < intNumberOfColumns; i++) 
+        {
+            arrRowNumbers[i] = (Math.floor(Math.random() * (max - min)) + min);
+            intArrayAverage = intArrayAverage + arrRowNumbers[i];
+        }
+        intArrayAverage = intArrayAverage / arrRowNumbers.length;
+
+        for (var i = 0; i < intNumberOfRows; i++) 
+        {
+            arrColumnNumbers [i] = (Math.floor(Math.random() * (max - intArrayAverage) + intArrayAverage));
+            document.write('<td>' + arrColumnNumbers [i] + '</td>');
+        }
+        document.write ('</tr>');
+        for (var i = 0; i < intNumberOfColumns; i++) 
+        {
+            intArrayAverage=0;
+            intCounter = 0;
+            document.write ('<tr>')
+            arrMatrix[i] = [];
+            document.write ('<td>' + arrRowNumbers[i] + '</td>');
+            for (var j = 0; j < intNumberOfRows; j++)
+            {
+                arrMatrix [i][j] = arrRowNumbers[i] * arrColumnNumbers[j];
+                intArrayAverage = intArrayAverage +  arrMatrix [i][j];
+                document.write('<td> a' + i + '*b' + j + ' = ' +arrMatrix [i][j] + '</td>');
+            }
+            document.write('</tr>');
+        }
+    }
+}
